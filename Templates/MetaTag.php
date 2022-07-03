3faaf88fd3f3618273854a70fbebb88c08de8f1e
Osotechcoding <osotechcoding@gmail.com>
2022-07-03 18:02:09 +0100

    Git Was initialized

---
+++ Templates/MetaTag.php
@@ -0,0 +1,5 @@
+<meta charset="utf-8">
+<meta name="description" content="">
+<!-- responsive tag -->
+<meta http-equiv="x-ua-compatible" content="ie=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1">
